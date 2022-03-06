56df9071c312baf7ac7875e82efcf2b570999fa2
Horsebye <73525155+Horsebye@users.noreply.github.com>
2022-03-06 17:14:16 +0100

    zadania lab2, całe

--- zad5.py
+++ zad5.py
@@ -4,11 +4,12 @@ f = open("liczby.txt", "w")
 f.write("3\n4\n5")
 
 f = open("liczby.txt", "r")
-liczba = f.readlines(3)
+liczba = f.readlines()
 
 lista = []
 
 for x in liczba:
+    lista.append(int(x.strip()))
 
+print((lista[0]**lista[1])+lista[2])
 
-print(liczba)
